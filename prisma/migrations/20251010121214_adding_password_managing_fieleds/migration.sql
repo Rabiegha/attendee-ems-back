2711732926d011b2813c7000d03017ade9d35ba7
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-10-10 14:14:04 +0200

    adding password managing fields

---
+++ prisma/migrations/20251010121214_adding_password_managing_fieleds/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[reset_token]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "must_change_password" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "reset_token" TEXT,
+ADD COLUMN     "reset_token_expires_at" TIMESTAMP(3);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_reset_token_key" ON "users"("reset_token");
